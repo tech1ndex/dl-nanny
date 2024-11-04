dc0e22b1fefa33d91400107ec60adbf320d33b64
Victor Bajada <bajada.victor@gmail.com>
2024-11-04 18:06:00 -0500

    fix env

--- src/dl-nanny/deluge.py
+++ src/dl-nanny/deluge.py
@@ -2,8 +2,8 @@ from deluge_client import DelugeRPCClient
 import os
 
 # Connect to the remote Deluge daemon
-HOST = "192.168.11.100"
-PORT = 58846
+HOST = os.environ.get('DELUGE_HOST')
+PORT = os.environ.get('DELUGE_PORT')
 USERNAME = os.environ.get('DELUGE_USERNAME')
 PASSWORD = os.environ.get('DELUGE_PASSWORD')
 
